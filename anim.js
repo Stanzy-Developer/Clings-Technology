f2855dba08f1434bae71f3274916031743c3fbc9
Stanzy-Developer <nnoupingnouping@gmail.com>
2023-02-12 20:24:57 +0100

    Linking the contact us form with FormSender

--- anim.js
+++ anim.js
@@ -15,3 +15,31 @@ scrollTop.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+function sendMail() {
+  var param = {
+    firstName: document.getElementById("firstName").value,
+    lastName: document.getElementById("lastName").value,
+    name: firstName + "" + lastName,
+    subject: document.getElementById("subject").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+  const serviceId = "stanzy237_dev";
+  const templateId = "template_vheu1y7";
+
+  emailjs
+    .sent(serviceId, templateId, params)
+    .then((res) => {
+      document.getElementById("firstName").value = "";
+      document.getElementById("lastName").value = "";
+      document.getElementById("subject").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+
+      console.log(res);
+      alert("Your message have been sent successfully");
+    })
+
+    .catch((err) => console.log(err));
+}
